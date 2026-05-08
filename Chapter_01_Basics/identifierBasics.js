82080618b0f90c494e6adcdd9937795a73629b62
Lavanya Kameshwaran <lavanyamuthukumarqa209@gmail.com>
2026-05-08 20:58:53 +0530

    Add identifierBasics example for snake_case and PascalCase

---
+++ Chapter_01_Basics/identifierBasics.js
@@ -0,0 +1,8 @@
+// snake_case identifier
+let user_name = "Alice";
+
+// PascalCase identifier
+let UserName = "Bob";
+
+console.log("snake_case:", user_name);
+console.log("PascalCase:", UserName);
