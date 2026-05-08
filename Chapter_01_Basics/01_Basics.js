9d1ca1c66b38062e854df7d274f6cb003672aa44
Lavanya Kameshwaran <lavanyamuthukumarqa209@gmail.com>
2026-05-08 20:54:14 +0530

    Add camelCase and snake_case examples

--- Chapter_01_Basics/01_Basics.js
+++ Chapter_01_Basics/01_Basics.js
@@ -2,3 +2,18 @@ console.log("Hello data ");
 let a=10;
 console.log("The value of a is ",a);
 
+// camelCase examples (JavaScript convention)
+let firstName = "John";
+let lastName = "Doe";
+let isLoggedIn = true;
+let getUserData = function() { return "user data"; };
+
+// snake_case examples
+let first_name = "Jane";
+let last_name = "Smith";
+let is_logged_in = false;
+let get_user_profile = function() { return "profile data"; };
+
+console.log("camelCase:", firstName, lastName, isLoggedIn);
+console.log("snake_case:", first_name, last_name, is_logged_in);
+
